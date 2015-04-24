69726022148171635b965d6bd704c1877f1cdb11
Shubhadeep Dhar <shubhadeep@gmail.com>
2015-04-25 00:16:26 +0530

    basic implementation of detail view, and fallback notfound view

---
+++ view/IssueDetail.controller.js
@@ -0,0 +1,17 @@
+/*global sap */
+sap.ui.core.mvc.Controller.extend("sap.ui.demo.tracker.view.IssueDetail", {
+  onInit: function () {
+    "use strict";
+    sap.ui.core.UIComponent.getRouterFor(this).attachRouteMatched(this.onRouteMatched, this);
+  },
+  onRouteMatched: function (e) {
+    "use strict";
+    var routeParameters = e.getParameters(),
+        issueBindingPath;
+    if (!routeParameters.name === "issue") {
+      return;
+    }
+    issueBindingPath = "/Issues(" + routeParameters.arguments.issueId + ")";
+    this.getView().bindElement(issueBindingPath);
+  }
+});
