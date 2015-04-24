63e7d320e7b6e8c888fa95e10b856ee9c0668a70
Shubhadeep Dhar <shubhadeep@gmail.com>
2015-04-24 16:57:49 +0530

    linting with eslint

--- Component.js
+++ Component.js
@@ -1,45 +1,47 @@
+/*global jQuery, sap */
 jQuery.sap.declare("sap.ui.demo.tracker.Component");
 
 sap.ui.core.UIComponent.extend("sap.ui.demo.tracker.Component", {
-  metadata : {
-    name : "Issue Tracker Demo App",
-    version : "1.0",
-    includes : [],
-    dependencies : {
-      libs : ["sap.m", "sap.ui.layout"],
-      components : []
+  metadata: {
+    name: "Issue Tracker Demo App",
+    version: "1.0",
+    includes: [],
+    dependencies: {
+      libs: ["sap.m", "sap.ui.layout"],
+      components: []
     },
 
-    rootView : "sap.ui.demo.tracker.view.App",
+    rootView: "sap.ui.demo.tracker.view.App",
 
-    config : {
-      resourceBundle : "i18n/messageBundle.properties",
-      serviceConfig : {
-        name : "Northwind",
-        serviceUrl : "/uilib-sample/proxy/http/services.odata.org/V2/(S(demotrackerapp))/OData/OData.svc/"
+    config: {
+      resourceBundle: "i18n/messageBundle.properties",
+      serviceConfig: {
+        name: "Northwind",
+        serviceUrl: "/uilib-sample/proxy/http/services.odata.org/V2/(S(demotrackerapp))/OData/OData.svc/"
       }
     },
 
-    routing : {
-      config : {
-        routerClass : sap.ui.core.routing.Router,
-        viewType : "XML",
-        viewPath : "sap.ui.demo.tracker.view",
-        targetAggregation : "pages",
-        targetControl : "idAppControl",
-        clearTarget : false
+    routing: {
+      config: {
+        routerClass: sap.ui.core.routing.Router,
+        viewType: "XML",
+        viewPath: "sap.ui.demo.tracker.view",
+        targetAggregation: "pages",
+        targetControl: "idAppControl",
+        clearTarget: false
       },
-      routes : [
+      routes: [
         {
-          pattern : "",
-          name : "main",
-          view : "IssueList"
+          pattern: "",
+          name: "main",
+          view: "IssueList"
         }
       ]
     }
   },
 
-  init : function() {
+  init: function() {
+    "use strict";
     sap.ui.core.UIComponent.prototype.init.apply(this, arguments);
 
     var mConfig = this.getMetadata().getConfig();
@@ -50,7 +52,7 @@ sap.ui.core.UIComponent.extend("sap.ui.demo.tracker.Component", {
 
     // set i18n model
     var i18nModel = new sap.ui.model.resource.ResourceModel({
-      bundleUrl : [rootPath, mConfig.resourceBundle].join("/")
+      bundleUrl: [rootPath, mConfig.resourceBundle].join("/")
     });
     this.setModel(i18nModel, "i18n");
 
--- view/IssueList.controller.js
+++ view/IssueList.controller.js
@@ -1,5 +1,7 @@
+/*global sap */
 sap.ui.core.mvc.Controller.extend("sap.ui.demo.tracker.view.IssueList", {
   handleDelete: function (e) {
+    "use strict";
     var oList = e.getSource(),
         sPath = e.getParameter("listItem").getBindingContext().getPath();
 
@@ -13,9 +15,11 @@ sap.ui.core.mvc.Controller.extend("sap.ui.demo.tracker.view.IssueList", {
     });
   },
   onDeleteIssueSuccess: function (data, response) {
+    "use strict";
     sap.m.MessageToast.show("Deleted");
   },
   onDeleteIssueError: function (error) {
+    "use strict";
     sap.m.MessageToast.show("Error");
   }
 });
