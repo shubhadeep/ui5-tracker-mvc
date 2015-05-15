42ee11ab9d79eacbaafd3180926f2d9484cd9768
Shubhadeep Dhar <shubhadeep@gmail.com>
2015-05-15 23:14:01 +0530

    moved Models from jQuery.sap.declare to sap.ui.define

--- model/CreateIssueModel.js
+++ model/CreateIssueModel.js
@@ -1,100 +1,96 @@
 /*global jQuery, sap*/
-jQuery.sap.declare("sap.ui.demo.tracker.model.CreateIssueModel");
+sap.ui.define(["sap/ui/model/json/JSONModel"],
+  function (JSONModel) {
+    "use strict";
 
-sap.ui.model.json.JSONModel.extend("sap.ui.demo.tracker.model.CreateIssueModel", {
-  data: {
-    priorities: [
-      { priority: "Select Priority", key: "" },
-      { priority: "High", key: "1" },
-      { priority: "Medium", key: "2" },
-      { priority: "Low", key: "3" }
-    ],
-    owners: [
-      { owner: "Select owner", key: "" },
-      { owner: "John Smith", key: "01" },
-      { owner: "Mary Jane", key: "02" },
-      { owner: "Jane Doe", key: "03" }
-    ],
-    newIssueObject: {
-      Name: "",
-      Description: "",
-      Priority: "",
-      Owner: ""
-    },
-    newIssueValueState: {
-      Name: sap.ui.core.ValueState.None,
-      Priority: sap.ui.core.ValueState.None
-    },
-    validationProperties: {
-      Name: {
-        validator: function (object, property, context) {},
-        errorMessage: "Name is required"
+    var model = JSONModel.extend("sap.ui.demo.tracker.model.CreateIssueModel", {
+      data: {
+        priorities: [
+          { priority: "Select Priority", key: "" },
+          { priority: "High", key: "1" },
+          { priority: "Medium", key: "2" },
+          { priority: "Low", key: "3" }
+        ],
+        owners: [
+          { owner: "Select owner", key: "" },
+          { owner: "John Smith", key: "01" },
+          { owner: "Mary Jane", key: "02" },
+          { owner: "Jane Doe", key: "03" }
+        ],
+        newIssueObject: {
+          Name: "",
+          Description: "",
+          Priority: "",
+          Owner: ""
+        },
+        newIssueValueState: {
+          Name: sap.ui.core.ValueState.None,
+          Priority: sap.ui.core.ValueState.None
+        },
+        validationProperties: {
+          Name: {
+            validator: function (object, property, context) {},
+            errorMessage: "Name is required"
+          },
+          Priority: {
+            validator: function (object, property, context) {},
+            errorMessage: "Priority is required"
+          }
+        },
+        fieldErrorMessages: {
+          Name: "Name is required", // TODO: Storing field messages in i18n model
+          Priority: "Priority is required"
+        }
       },
-      Priority: {
-        validator: function (object, property, context) {},
-        errorMessage: "Priority is required"
+
+      initializeNewIssue: function () {
+        this.setProperty("/newIssueObject", {
+          Name: "",
+          Description: "",
+          Priority: "",
+          Owner: ""
+        });
+
+        this.setProperty("/newIssueValueState", {
+          Name: sap.ui.core.ValueState.None,
+          Priority: sap.ui.core.ValueState.None // Does not work !
+        });
+      },
+      getNewIssueObject: function () {
+        var newIssueObject = jQuery.extend(this.getProperty("/newIssueObject"), {});
+        newIssueObject.Created = new Date();
+        return newIssueObject;
+      },
+      validate: function (onValid, onInvalid, context) {
+        var validated = jQuery.Deferred(),
+            inputObject = this.getNewIssueObject(),
+            errors = {},
+            valid,
+            callbackContext = context || this,
+            properties = ["Name", "Priority"];
+
+        valid = properties.map(function (property) {
+          if (!this.isNonEmptyStringProperty(property, inputObject)) {
+            errors[property] = this.data.fieldErrorMessages[property];
+            return false;
+          }
+          return true;
+        }, this).every(function (isValid) {
+          return isValid;
+        });
+
+        if (valid) {
+          validated.resolveWith(callbackContext, [inputObject]);
+        } else {
+          validated.rejectWith(callbackContext, [errors]);
+        }
+        return validated.promise();
+      },
+      isNonEmptyStringProperty: function (property, object) {
+        return ((property in object) &&
+                (object[property].toString().trim().length > 0));
       }
-    },
-    fieldErrorMessages: {
-      Name: "Name is required", // TODO: Storing field messages in i18n model
-      Priority: "Priority is required"
-    }
-  },
-
-  initializeNewIssue: function () {
-    "use strict";
-
-    this.setProperty("/newIssueObject", {
-      Name: "",
-      Description: "",
-      Priority: "",
-      Owner: ""
     });
 
-    this.setProperty("/newIssueValueState", {
-      Name: sap.ui.core.ValueState.None,
-      Priority: sap.ui.core.ValueState.None // Does not work !
-    });
-
-  },
-  getNewIssueObject: function () {
-    "use strict";
-
-    var newIssueObject = jQuery.extend(this.getProperty("/newIssueObject"), {});
-    newIssueObject.Created = new Date();
-    return newIssueObject;
-  },
-  validate: function (onValid, onInvalid, context) {
-    "use strict";
-
-    var validated = jQuery.Deferred(),
-        inputObject = this.getNewIssueObject(),
-        errors = {},
-        valid,
-        callbackContext = context || this,
-        properties = ["Name", "Priority"];
-
-    valid = properties.map(function (property) {
-      if (!this.isNonEmptyStringProperty(property, inputObject)) {
-        errors[property] = this.data.fieldErrorMessages[property];
-        return false;
-      }
-      return true;
-    }, this).every(function (isValid) {
-      return isValid;
-    });
-
-    if (valid) {
-      validated.resolveWith(callbackContext, [inputObject]);
-    } else {
-      validated.rejectWith(callbackContext, [errors]);
-    }
-    return validated.promise();
-  },
-  isNonEmptyStringProperty: function (property, object) {
-    "use strict";
-
-    return ((property in object) &&
-            (object[property].toString().trim().length > 0));
-  }
-});
+    return model;
+  }, true /* export */);
--- model/IssueModel.js
+++ model/IssueModel.js
@@ -1,47 +1,42 @@
 /*global jQuery, sap*/
-jQuery.sap.declare("sap.ui.demo.tracker.model.IssueModel");
-
-sap.ui.model.odata.v2.ODataModel.extend("sap.ui.demo.tracker.model.IssueModel", {
-  getEntitySetPath: function () {
+sap.ui.define(["sap/ui/model/odata/v2/ODataModel"],
+  function (ODataModel) {
     "use strict";
 
-    return "/Issues";
-  },
-  createNew: function () {
-    "use strict";
+    var model = ODataModel.extend("sap.ui.demo.tracker.model.IssueModel", {
+      getEntitySetPath: function () {
+        return "/Issues";
+      },
+      createNew: function () {
+        var oldArgs = Array.prototype.slice.call(arguments),
+            newArgs = [this.getEntitySetPath()].concat(oldArgs);
 
-    var oldArgs = Array.prototype.slice.call(arguments),
-        newArgs = [this.getEntitySetPath()].concat(oldArgs);
+        return this.create.apply(this, newArgs);
+      },
+      updateExisting: function (obj) {
+        var oldArgs = Array.prototype.slice.call(arguments),
+            path = this.getBindingPathById(obj.ID),
+            newArgs = [path].concat(oldArgs);
 
-    return this.create.apply(this, newArgs);
-  },
-  updateExisting: function (obj) {
-    "use strict";
+        return this.update.apply(this, newArgs);
+      },
+      getIdByBindingPath: function (bindingPath) {
+        var parts = bindingPath.split("(");
 
-    var oldArgs = Array.prototype.slice.call(arguments),
-        path = this.getBindingPathById(obj.ID),
-        newArgs = [path].concat(oldArgs);
+        if (parts.length > 1) {
+          return parts[1].split(")")[0];
+        }
 
-    return this.update.apply(this, newArgs);
-  },
-  getIdByBindingPath: function (bindingPath) {
-    "use strict";
+        return "";
+      },
+      getBindingPathById: function (id) {
+        if ((id !== undefined) && (id.toString().trim().length > 0)) {
+          return jQuery.sap.formatMessage("{0}({1})", [this.getEntitySetPath(), id]);
+        }
 
-    var parts = bindingPath.split("(");
+        return "";
+      }
+    });
 
-    if (parts.length > 1) {
-      return parts[1].split(")")[0];
-    }
-
-    return "";
-  },
-  getBindingPathById: function (id) {
-    "use strict";
-
-    if ((id !== undefined) && (id.toString().trim().length > 0)) {
-      return jQuery.sap.formatMessage("{0}({1})", [this.getEntitySetPath(), id]);
-    }
-
-    return "";
-  }
-});
+    return model;
+  }, true /* export */);
