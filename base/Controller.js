0848940b2c22ee29e5a001a11e307fd1d2fe69fb
Shubhadeep Dhar <shubhadeep@gmail.com>
2015-05-17 22:38:10 +0530

    Base controller now uses asynchronous module definition

--- base/Controller.js
+++ base/Controller.js
@@ -1,45 +1,40 @@
 /*global window sap jQuery */
-jQuery.sap.declare("sap.ui.demo.tracker.base.Controller");
-
-sap.ui.core.mvc.Controller.extend("sap.ui.demo.tracker.base.Controller", {
-  getRouter: function () {
+sap.ui.define(
+  ['sap/ui/core/mvc/Controller',
+   'sap/m/MessageBox'], 
+  function (Controller, MessageBox) {
     "use strict";
 
-    return  sap.ui.core.UIComponent.getRouterFor(this);
-  },
-  displayValidationErrors: function () {
-    "use strict";
-
-    jQuery.sap.require("sap.m.MessageBox");
-    sap.m.MessageBox.show("Validation Error", {
-      title: "Invalid Inputs"
-    });
-  },
-  showMessageToast: function (message) {
-    "use strict";
-
-    window.setTimeout(function () {
-        sap.m.MessageToast.show(message);
-      }, 0);
-  },
-  showBackendError: function (error) {
-    "use strict";
-
-    jQuery.sap.require("sap.m.MessageBox");
-    sap.m.MessageBox.show(
-      error.responseText, {
-        icon: sap.m.MessageBox.Icon.ERROR,
-        title: error.message,
-        actions: [sap.m.MessageBox.Action.OK]
+    var controller = Controller.extend("sap.ui.demo.tracker.base.Controller", {
+      getRouter: function () {
+        return  sap.ui.core.UIComponent.getRouterFor(this);
+      },
+      displayValidationErrors: function () {
+        MessageBox.show("Validation Error", {
+          title: "Invalid Inputs"
+        });
+      },
+      showMessageToast: function (message) {
+        window.setTimeout(function () {
+            sap.m.MessageToast.show(message);
+          }, 0);
+      },
+      showBackendError: function (error) {
+        MessageBox.show(
+          error.responseText, {
+            icon: sap.m.MessageBox.Icon.ERROR,
+            title: error.message,
+            actions: [sap.m.MessageBox.Action.OK]
+          }
+        );
+      },
+      getI18nText: function (key) {
+        return this.getOwnerComponent()
+                   .getModel("i18n")
+                   .getResourceBundle()
+                   .getText(key);
       }
-    );
-  },
-  getI18nText: function (key) {
-    "use strict";
+    });
 
-    return this.getOwnerComponent()
-               .getModel("i18n")
-               .getResourceBundle()
-               .getText(key);
-  }
-});
+    return controller;
+  }, true /*export*/);
