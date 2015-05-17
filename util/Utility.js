102f33c41d0f62f325e6daa985582db23b67d8d3
Shubhadeep Dhar <shubhadeep@gmail.com>
2015-05-17 22:42:36 +0530

    Utility is defined as AMD

--- util/Utility.js
+++ util/Utility.js
@@ -1,12 +1,13 @@
 /*global window sap jQuery */
-jQuery.sap.declare("sap.ui.demo.tracker.util.Utility");
+sap.ui.define([], function () {
+  "use strict";
 
-sap.ui.demo.tracker.util.Utility = {
-  displayMessageToast: function (message) {
-    "use strict";
+  sap.ui.demo.tracker.util.Utility = {
+    displayMessageToast: function (message) {
+      window.setTimeout(function () {
+        sap.m.MessageToast.show(message);
+      }, 0);
+    }
+  };
 
-    window.setTimeout(function () {
-      sap.m.MessageToast.show(message);
-    }, 0);
-  }
-};
+}, true /*export*/);
