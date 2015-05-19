e701a6ccfa09ac28caa4a7e33f549d218ec12d6c
Shubhadeep Dhar <shubhadeep@gmail.com>
2015-05-19 23:53:41 +0530

    Move to AMD style require

--- Component.js
+++ Component.js
@@ -1,139 +1,131 @@
 /*global jQuery, sap */
-jQuery.sap.declare("sap.ui.demo.tracker.Component");
-jQuery.sap.require("sap.ui.demo.tracker.model.IssueModel");
-jQuery.sap.require("sap.ui.demo.tracker.util.Utility");
+sap.ui.define([
+  "sap/ui/demo/tracker/model/IssueModel",
+  "sap/ui/demo/tracker/util/Utility"],
+  function (IssueModel, Utility) {
+    "use strict";
 
-sap.ui.core.UIComponent.extend("sap.ui.demo.tracker.Component", {
-  metadata: {
-    name: "Issue Tracker Demo App",
-    version: "1.0",
-    includes: [],
-    dependencies: {
-      libs: ["sap.m", "sap.ui.layout"],
-      components: []
-    },
+    var component = sap.ui.core.UIComponent.extend("sap.ui.demo.tracker.Component", {
+      metadata: {
+        name: "Issue Tracker Demo App",
+        version: "1.0",
+        includes: [],
+        dependencies: {
+          libs: ["sap.m", "sap.ui.layout"],
+          components: []
+        },
 
-    rootView: "sap.ui.demo.tracker.view.App",
+        rootView: "sap.ui.demo.tracker.view.App",
 
-    config: {
-      resourceBundle: "i18n/messageBundle.properties",
-      serviceConfig: {
-        name: "Northwind",
-        serviceUrl: "/OData/OData.svc/"
-      }
-    },
+        config: {
+          resourceBundle: "i18n/messageBundle.properties",
+          serviceConfig: {
+            name: "Northwind",
+            serviceUrl: "/OData/OData.svc/"
+          }
+        },
 
-    routing: {
-      config: {
-        routerClass: sap.ui.core.routing.Router,
-        viewType: "XML",
-        viewPath: "sap.ui.demo.tracker.view",
-        targetAggregation: "pages",
-        targetControl: "idAppControl",
-        clearTarget: false
-      },
-      routes: [
-        {
-          pattern: "",
-          name: "list",
-          view: "IssueList",
-          viewLevel: 0
-        },
-        {
-          pattern: "issue/{issueId}",
-          name: "detail",
-          view: "IssueDetail",
-          viewLevel: 1
-        },
-        {
-          pattern: "edit/{issueId}",
-          name: "edit",
-          view: "IssueEdit",
-          viewLevel: 1
-        },
-        {
-          pattern: "create",
-          name: "create",
-          view: "IssueCreate",
-          viewLevel: 1
-        },
-        {
-          name: "catchAllMaster",
-          view: "NotFound",
-          pattern: ":all*:"
+        routing: {
+          config: {
+            routerClass: sap.ui.core.routing.Router,
+            viewType: "XML",
+            viewPath: "sap.ui.demo.tracker.view",
+            targetAggregation: "pages",
+            targetControl: "idAppControl",
+            clearTarget: false
+          },
+          routes: [
+            {
+              pattern: "",
+              name: "list",
+              view: "IssueList",
+              viewLevel: 0
+            },
+            {
+              pattern: "issue/{issueId}",
+              name: "detail",
+              view: "IssueDetail",
+              viewLevel: 1
+            },
+            {
+              pattern: "edit/{issueId}",
+              name: "edit",
+              view: "IssueEdit",
+              viewLevel: 1
+            },
+            {
+              pattern: "create",
+              name: "create",
+              view: "IssueCreate",
+              viewLevel: 1
+            },
+            {
+              name: "catchAllMaster",
+              view: "NotFound",
+              pattern: ":all*:"
+            }
+          ]
         }
-      ]
-    }
-  },
+      },
 
-  init: function() {
-    "use strict";
+      init: function() {
+        var mConfig, sServiceUrl;
 
-    var mConfig, sServiceUrl;
+        sap.ui.core.UIComponent.prototype.init.apply(this, arguments);
 
-    sap.ui.core.UIComponent.prototype.init.apply(this, arguments);
+        mConfig = this.getMetadata()
+                      .getConfig();
 
-    mConfig = this.getMetadata()
-                  .getConfig();
+        this.setI18nModel(mConfig.resourceBundle);
 
-    this.setI18nModel(mConfig.resourceBundle);
+        sServiceUrl = mConfig.serviceConfig.serviceUrl;
 
-    sServiceUrl = mConfig.serviceConfig.serviceUrl;
+        if (this.useMockData()) {
+          this.startMockServer(sServiceUrl);
+        }
 
-    if (this.useMockData()) {
-      this.startMockServer(sServiceUrl);
-    }
+        this.setModel(new IssueModel(sServiceUrl, true));
 
-    this.setModel(new sap.ui.demo.tracker.model.IssueModel(sServiceUrl, true));
+        this.initializeRouter(this.getRouter());
+      },
+      destroy: function () {
+        if (this.routeHandler) {
+          this.routeHandler.destroy();
+        }
+        sap.ui.core.UIComponent.destroy.apply(this, arguments);
+      },
+      setI18nModel: function (resourceBundle) {
+        // always use absolute paths relative to our own component
+        // (relative paths will fail if running in the Fiori Launchpad)
+        var rootPath = jQuery.sap.getModulePath("sap.ui.demo.tracker"),
+            i18nModel = new sap.ui.model.resource.ResourceModel({
+              bundleUrl: [rootPath, resourceBundle].join("/")
+            });
 
-    this.initializeRouter(this.getRouter());
-  },
-  destroy: function () {
-    "use strict";
+        this.setModel(i18nModel, "i18n");
+      },
+      useMockData: function () {
+        return jQuery.sap.getUriParameters().get("responderOn") === "true";
+      },
+      startMockServer: function (serviceUrl) {
+        var mockServer;
 
-    if (this.routeHandler) {
-      this.routeHandler.destroy();
-    }
-    sap.ui.core.UIComponent.destroy.apply(this, arguments);
-  },
-  setI18nModel: function (resourceBundle) {
-    "use strict";
-
-    // always use absolute paths relative to our own component
-    // (relative paths will fail if running in the Fiori Launchpad)
-    var rootPath = jQuery.sap.getModulePath("sap.ui.demo.tracker"),
-        i18nModel = new sap.ui.model.resource.ResourceModel({
-          bundleUrl: [rootPath, resourceBundle].join("/")
+        jQuery.sap.require("sap.ui.app.MockServer");
+        mockServer = new sap.ui.app.MockServer({
+          rootUri: serviceUrl
         });
 
-    this.setModel(i18nModel, "i18n");
-  },
-  useMockData: function () {
-    "use strict";
+        mockServer.simulate("model/metadata.xml", "model/");
+        mockServer.start();
 
-    return jQuery.sap.getUriParameters().get("responderOn") === "true";
-  },
-  startMockServer: function (serviceUrl) {
-    "use strict";
+        Utility.displayMessageToast("Running in demo mode with mock data.");
 
-    var util = sap.ui.demo.tracker.util.Utility,
-        mockServer;
-
-    jQuery.sap.require("sap.ui.app.MockServer");
-    mockServer = new sap.ui.app.MockServer({
-      rootUri: serviceUrl
+      },
+      initializeRouter: function (router) {
+        this.routeHandler = new sap.m.routing.RouteMatchedHandler(router);
+        router.initialize();
+      }
     });
 
-    mockServer.simulate("model/metadata.xml", "model/");
-    mockServer.start();
-
-    util.displayMessageToast("Running in demo mode with mock data.");
-
-  },
-  initializeRouter: function (router) {
-    "use strict";
-
-    this.routeHandler = new sap.m.routing.RouteMatchedHandler(router);
-    router.initialize();
-  }
-});
+    return component;
+  }, true /*export*/);
