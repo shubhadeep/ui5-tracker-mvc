a9513f89e042f855fa38c346d39b9b75903be7d9
Shubhadeep Dhar <shubhadeep@gmail.com>
2015-07-22 17:10:17 +0530

    added handling of issue not found

--- view/IssueDetail.controller.js
+++ view/IssueDetail.controller.js
@@ -1,16 +1,23 @@
 /*global sap */
 sap.ui.define(
   ["sap/ui/demo/tracker/base/Controller",
-   "sap/ui/demo/tracker/util/Utility"],
-  function (Controller, Utility) {
+   "sap/ui/demo/tracker/util/Utility",
+   "sap/ui/model/json/JSONModel",
+   "sap/m/MessageBox"],
+  function (Controller, Utility, JSONModel, MessageBox) {
     "use strict";
 
     var controller = Controller.extend("sap.ui.demo.tracker.view.IssueDetail", {
+
       onInit: function () {
         this.getRouter()
             .getRoute("detail")
             .attachMatched(this.onRouteMatched, this);
+
+        this.getView()
+            .setModel(new JSONModel({}), "detail");
       },
+
       onRouteMatched: function (e) {
         var routeParameters = e.getParameters(),
             issueBindingPath,
@@ -22,8 +29,13 @@ sap.ui.define(
         issueBindingPath = view.getModel()
                                .getBindingPathById(issueId);
 
-        view.bindElement(issueBindingPath);
+        view.getModel()
+            .read(issueBindingPath, {
+              success: this.onIssueDataLoaded.bind(this),
+              error: this.onIssueDataLoadFailed.bind(this)
+            });
       },
+
       handleEditPress: function (e) {
         var issuePath = e.getSource()
                          .getBindingContext()
@@ -37,6 +49,7 @@ sap.ui.define(
               issueId: issueId
             });
       },
+
       handleDeletePress: function (e) {
         var issuePath = e.getSource()
                          .getBindingContext()
@@ -44,6 +57,25 @@ sap.ui.define(
 
         this.deleteIssue(issuePath);
       },
+
+      onIssueDataLoaded: function (data) {
+        this.getView()
+            .getModel("detail")
+            .setProperty("/", data);
+      },
+
+      onIssueDataLoadFailed: function (error) {
+        MessageBox.error(this.getI18nText("ISSUE_DETAIL_LOAD_FAIL"), {
+          onClose: this.goToList.bind(this) 
+          }
+        );
+      },
+
+      goToList: function () {
+        this.getRouter()
+            .navTo("list");
+      },
+
       deleteIssue: function (issueContextPath) {
         var model = this.getView()
                         .getModel();
@@ -54,6 +86,7 @@ sap.ui.define(
           error: this.showBackendError
         });
       },
+
       onIssueDeleted: function () {
         var message = this.getI18nText("ISSUE_DELETE_SUCCESS_MESSAGE");
 
